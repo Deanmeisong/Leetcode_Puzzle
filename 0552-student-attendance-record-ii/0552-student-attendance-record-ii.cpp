da03708496eaaedb5de090aa6a79d9f1ce85f8b5
Deanmeisong <deanmeisong@gmail.com>
2023-07-15 18:31:31 +1000

    Time: 2648 ms (17.69%), Space: 418.9 MB (26.90%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int checkRecord(int n) {
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
-        dp[0][0][0] = 1;
+        vector<vector<int>> dp(2, vector<int>(3, 0));
+        dp[0][0] = 1;
         int kMod = 1e9 + 7;
         int ans = 0;
         
         for(int i = 1; i <= n; ++i) {
             // if p
+            vector<vector<int>> tmp(2, vector<int>(3, 0));
             for(int j = 0; j < 2; ++j)
                 for(int k = 0; k < 3; ++k) 
-                    dp[i][j][0] = (dp[i][j][0] + dp[i - 1][j][k]) % kMod;
+                    tmp[j][0] = (tmp[j][0] + dp[j][k]) % kMod;
             
             // if a
-            for(int k = 0; k < 3; ++k) dp[i][1][0] = (dp[i][1][0] + dp[i - 1][0][k]) % kMod;
+            for(int k = 0; k < 3; ++k) tmp[1][0] = (tmp[1][0] + dp[0][k]) % kMod;
             for(int j = 0; j < 2; ++j)
-                for(int k = 1; k < 3; ++k) dp[i][j][k] = (dp[i][j][k] + dp[i - 1][j][k - 1]) % kMod;
+                for(int k = 1; k < 3; ++k) tmp[j][k] = (tmp[j][k] + dp[j][k - 1]) % kMod;
+            
+            dp = tmp;
         }
         
         for(int j = 0; j < 2; ++j)
             for(int k = 0; k < 3; ++k)
-                ans = (ans + dp[n][j][k]) % kMod;
+                ans = (ans + dp[j][k]) % kMod;
     
         return ans;
         
