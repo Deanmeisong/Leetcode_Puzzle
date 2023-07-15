77fe0610372ccfc1c27fc540403d854bcf16f8f7
Deanmeisong <deanmeisong@gmail.com>
2023-07-15 16:34:53 +1000

    Time: 83 ms (98.11%), Space: 36.3 MB (16.04%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int[] ConstructRectangle(int area) {
+        for(int i = (int)Math.Sqrt(area); i > 0; --i) {
+            if(area % i == 0) return new int[]{area / i, i};
+        }
+        return new int[]{area, 1};
+    }
+}
