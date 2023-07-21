924d797a0f7391f073951c2d96628dacf6cf5b17
Deanmeisong <deanmeisong@gmail.com>
2023-07-21 23:19:24 +1000

    Time: 1 ms (75.65%), Space: 43.8 MB (88.81%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+//         int[] nums = new int[n];
+//         Arrays.fill(nums, -1);
+        
+//         for(int[] e : edges) {
+//             int x = find(nums, e[0]);
+//             int y = find(nums, e[1]);
+            
+//             if(x == y) return false;
+            
+//             x = nums[y];
+//         }
+        
+//         return n - 1 == edges.length;
+//     }
+    
+//     int find(int[] nums, int i) {
+//         if(nums[i] == -1) return i;
+//         return find(nums, nums[i]);
+                // initialize n isolated islands
+        int[] nums = new int[n];
+        Arrays.fill(nums, -1);
+        
+        // perform union find
+        for (int i = 0; i < edges.length; i++) {
+            int x = find(nums, edges[i][0]);
+            int y = find(nums, edges[i][1]);
+            
+            // if two vertices happen to be in the same set
+            // then there's a cycle
+            if (x == y) return false;
+            
+            // union
+            nums[x] = y;
+        }
+        
+        return edges.length == n - 1;
+    }
+    
+    int find(int nums[], int i) {
+        if (nums[i] == -1) return i;
+        return find(nums, nums[i]);
+    }
+    
+}
