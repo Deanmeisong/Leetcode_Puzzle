f60858e0e451b3f5fed92ef373795a0b683ba3d8
Deanmeisong <deanmeisong@gmail.com>
2023-07-24 22:11:36 +1000

    Time: 105 ms (94.17%), Space: 36.3 MB (84.15%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,13 +1,16 @@
 class MyCalendar {
 public:
-    vector<pair<int, int>> overLaps;
-    bool check(pair<int, int> meeting) {
-        for(auto o : overLaps) {
-            if(max(o.first, meeting.first) < min(o.second, meeting.second)) {
-                return false;
-            }
-        }
-        overLaps.push_back(meeting);
+    set<pair<int, int>> books;
+    bool check(int start, int end) {
+        // auto it = books.lower_bound({start, end});
+        // if(it != books.end() and it->first < end) return false;
+        // if(it != books.begin() and start < (--it)->second) return false;
+        // books.insert({start, end});
+        // return true;
+        auto it = books.lower_bound({start, end});
+        if(it != books.end() and end > it->first) return false;
+        if(it!= books.begin() and start < (--it)->second) return false;
+        books.insert({start, end});
         return true;
     }
 };
@@ -18,15 +21,16 @@ public:
     MyCalendarTwo() {}
     
     bool book(int start, int end) {
-        MyCalendar overlap;
-        for(auto book : books) {
-            if(max(book.first, start) < min(book.second, end)) {
-                auto p = getPair(start, end, book.first, book.second);
-                if(!overlap.check(p)) return false;
+        MyCalendar overLap;
+        for(auto b : books) {
+            if(max(start, b.first) < min(end, b.second)) {
+                auto p = getPair(start, end, b.first, b.second);
+                if(!overLap.check(p.first, p.second)) return false;
             }
         }
         books.push_back({start, end});
         return true;
+        
     }
     
     pair<int, int> getPair(int s1, int e1, int s2, int e2) {
