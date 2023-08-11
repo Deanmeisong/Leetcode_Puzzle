e1930e751347acb35542e52b3a18c7320a7fd386
Deanmeisong <deanmeisong@gmail.com>
2023-08-11 15:08:57 +1000

    Time: 2 ms (57.58%), Space: 8.9 MB (6.06%) - LeetHub

---
+++ 0776-split-bst/0776-split-bst.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> splitBST(TreeNode* root, int t) {
+        if(!root) return {nullptr, nullptr};
+        if(root->val <= t) {
+            auto res = splitBST(root->right, t);
+            root->right = res[0];
+            return {root, res[1]};
+        } else {
+            auto res = splitBST(root->left, t);
+            root->left = res[1];
+            return {res[0], root};
+        }
+    }
+};
