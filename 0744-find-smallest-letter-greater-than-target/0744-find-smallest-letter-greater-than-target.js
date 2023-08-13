7beb26c7928f4492061ec064933219d1a9031df2
Deanmeisong <deanmeisong@gmail.com>
2023-08-13 21:33:33 +1000

    Time: 50 ms (96.53%), Space: 43.7 MB (76.92%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -0,0 +1,18 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+        let left = 0, right = letters.length - 1, mid;
+        while (left <= right) {
+            mid = (left + right) // 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left == letters.length ? letters[0] : letters[left];
+};
