345d7792ce2d728a9eedbc6c2f45a530365055ff
Deanmeisong <deanmeisong@gmail.com>
2023-08-24 22:07:10 +1000

    Time: 0 ms (100.00%), Space: 7.2 MB (23.08%) - LeetHub

--- 1183-maximum-number-of-ones/1183-maximum-number-of-ones.cpp
+++ 1183-maximum-number-of-ones/1183-maximum-number-of-ones.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int maximumNumberOfOnes(int width, int height, int sideLength, int maxOnes) {
-        vector<int> count;
+        priority_queue<int> pq;
         for(int r = 0; r < sideLength; ++r)
             for(int c = 0; c < sideLength; ++c)
-                count.push_back((1 + (width - 1 - r) / sideLength) * (1 + (height - 1 - c) / sideLength));
-        
-        sort(count.begin(), count.end(), greater <>());
-        
+                pq.push((1 + (width - 1 - r) / sideLength) * (1 + (height - 1 - c) / sideLength));
+                
         int res = 0;
-        for(int i = 0; i < maxOnes; ++i) res += count[i];
+        int i = 0;
+        while(!pq.empty() && i < maxOnes) {
+            res += pq.top(); pq.pop(); ++i;
+        }
         return res;
     }
 };
