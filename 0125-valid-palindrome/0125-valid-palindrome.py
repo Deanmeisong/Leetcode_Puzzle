88dc4f9dc3b14a13446c1b0cee7bcb28246832af
Deanmeisong <deanmeisong@gmail.com>
2023-08-25 22:31:33 +1000

    Time: 31 ms (99.88%), Space: 17.1 MB (63.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         l = 0
         n = len(s)
         r = n - 1
