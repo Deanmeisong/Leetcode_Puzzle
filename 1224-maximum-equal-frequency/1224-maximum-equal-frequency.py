59ab557b4924c3370c81ccc5ffa643bdb41cf354
Deanmeisong <deanmeisong@gmail.com>
2023-08-26 23:21:52 +1000

    Time: 358 ms (96.80%), Space: 23.8 MB (91.20%) - LeetHub

--- 1224-maximum-equal-frequency/1224-maximum-equal-frequency.py
+++ 1224-maximum-equal-frequency/1224-maximum-equal-frequency.py
@@ -1,22 +1,13 @@
-class Solution(object):
-    def maxEqualFreq(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        count = [0] * 100001
-        freq = [0] * 100001
-        n = len(nums)
-        
+class Solution:
+    def maxEqualFreq(self, nums: List[int]) -> int:
+        count, freq = [0] * 100001, [0] * 100001
         for num in nums:
             count[num] += 1
             freq[count[num]] += 1
-            
+        n = len(nums)
         for i in range(n - 1, 0, -1):
-            if count[nums[i]] * freq[count[nums[i]]] == i:
-                return i + 1
+            if freq[count[nums[i]]] * count[nums[i]] == i: return i + 1
             freq[count[nums[i]]] -= 1
             count[nums[i]] -= 1
-            if count[nums[i - 1]] * freq[count[nums[i - 1]]] == i:
-                return i + 1
+            if freq[count[nums[i - 1]]] * count[nums[i - 1]] == i: return i + 1
         return 1
