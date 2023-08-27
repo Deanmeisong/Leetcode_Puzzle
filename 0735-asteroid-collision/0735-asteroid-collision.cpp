9a013e8f63875e98f719849900e7ebbc71ec33fd
Deanmeisong <deanmeisong@gmail.com>
2023-08-27 22:01:00 +1000

    Time: 11 ms (87.05%), Space: 17.8 MB (22.20%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
+    vector<int> asteroidCollision(vector<int>& A) {
         stack<int> st;
-        
-        for (int asteroid : asteroids) {
+        for(int a : A) {
             int flag = 1;
-            while (!st.empty() && (st.top() > 0 && asteroid < 0)) {
-                if (abs(st.top()) < abs(asteroid)) {
-                    st.pop();
-                    continue;
+            while(!st.empty() && (st.top() > 0 && a < 0)) {
+                if(abs(a) > abs(st.top())) {
+                    st.pop(); continue;
                 }
-                else if (abs(st.top()) == abs(asteroid)) {
+                if(abs(a) == abs(st.top())) {
                     st.pop();
                 }
                 flag = 0;
                 break;
             }
-            if (flag) {
-                st.push(asteroid);
-            }
+            if(flag) st.push(a);
         }
         
-        vector<int> remainingAsteroids (st.size());
-        for (int i = remainingAsteroids.size() - 1; i >= 0; i--){
-            remainingAsteroids[i] = st.top();
-            st.pop();
+        vector<int> ans(st.size());
+        for(int i = st.size() - 1; i >= 0; --i) {
+            ans[i] = st.top(); st.pop();
         }
         
-        return remainingAsteroids;
+        return ans;
     }
 };
