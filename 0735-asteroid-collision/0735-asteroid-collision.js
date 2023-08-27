5aeb8774667974947a5b801e9d4b0ad904ddee8a
Deanmeisong <deanmeisong@gmail.com>
2023-08-27 22:05:02 +1000

    Time: 73 ms (48.70%), Space: 44.3 MB (40.12%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function(A) {
+    let st = [];
+    for (let a of A) {
+        let flag = 1;
+        while (st.length > 0 && (st[st.length - 1] > 0 && a < 0)) {
+            if (Math.abs(a) > Math.abs(st[st.length - 1])) {
+                st.pop();
+                continue;
+            }
+            if (Math.abs(a) === Math.abs(st[st.length - 1])) {
+                st.pop();
+            }
+            flag = 0;
+            break;
+        }
+        if (flag === 1) {
+            st.push(a);
+        }
+    }
+    let ans = new Array(st.length);
+    for (let i = st.length - 1; i >= 0; --i) {
+        ans[i] = st[st.length - 1];
+        st.pop();
+    }
+    return ans;
+};
