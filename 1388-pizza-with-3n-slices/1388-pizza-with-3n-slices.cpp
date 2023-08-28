ef31b5ad0fc0d5654febe19d85764564ac8777f7
Deanmeisong <deanmeisong@gmail.com>
2023-08-28 22:41:01 +1000

    Time: 10 ms (98.04%), Space: 8.2 MB (98.77%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -6,40 +6,16 @@ public:
     }
     
     int helper(int start, int end, int k, vector<int>& slices) {
-
-        // int dp0[501][501]; 
-        // int dp1[501][501];
-
-        // for(int i = 0; i <= 500; ++i)
-        //     for(int j = 0; j <= 500; ++j) {
-        //         dp0[i][j] = 0; dp1[i][j] = 0;
-        //     }
-
+        // vector<vector<int>> dp0(501, vector<int>(k + 1, 0));
+        // vector<vector<int>> dp1(501, vector<int>(k + 1, 0));
+        vector<int> dp0(k + 1); vector<int> dp1(k + 1);
         
-        vector<vector<int>> dp0(501, vector<int>(k + 1, 0));
-        vector<vector<int>> dp1(501, vector<int>(k + 1, 0));
-        
-        dp0[start][0] = 0;
-        dp1[start][1] = slices[start];
-
-        for(int i = start + 1; i <= end; ++i)
-            for(int j = 1; j <= min(k, i - start + 1); ++j) {
-                dp0[i][j] = max(dp0[i - 1][j], dp1[i - 1][j]);
-                dp1[i][j] = dp0[i - 1][j - 1] + slices[i];
+        for(int i = start; i <= end; ++i)
+            for(int j = min(k, i - start + 1); j >= 1; --j) {
+                dp0[j] = max(dp0[j], dp1[j]);
+                dp1[j] = dp0[j - 1] + slices[i];
             }
         
-        return max(dp0[end][k], dp1[end][k]);
-        
-//              vector<int>dp0(k+1);
-//         vector<int>dp1(k+1);
-        
-//         for (int i=start; i<=end; i++)
-//             for (int j=min(k,i-start+1); j>=1; j--)
-//             {
-//                 dp0[j] = max(dp0[j], dp1[j]);
-//                 dp1[j] = dp0[j-1] + slices[i];
-//             }
-        
-//         return max(dp0[k], dp1[k]);
+        return max(dp0[k], dp1[k]);
     }
 };
