653bb887248ec2502db7461393f3223333b4ebd9
Deanmeisong <deanmeisong@gmail.com>
2023-10-18 17:46:48 +1100

    Time: 963 ms (75.00%), Space: 182.6 MB (87.50%) - LeetHub

--- 1956-minimum-time-for-k-virus-variants-to-spread/1956-minimum-time-for-k-virus-variants-to-spread.cpp
+++ 1956-minimum-time-for-k-virus-variants-to-spread/1956-minimum-time-for-k-virus-variants-to-spread.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int minDayskVariants(vector<vector<int>>& points, int k) {
-        constexpr int kMax = 100;
         int ans = INT_MAX;
-
-        for (int a = 1; a <= kMax; ++a)
-          for (int b = 1; b <= kMax; ++b) {
-            // Store the k smallest distances of points that can reach (a, b).
-            priority_queue<int> maxHeap;
-            for (const vector<int>& point : points) {
-              const int x = point[0];
-              const int y = point[1];
-              maxHeap.push(abs(x - a) + abs(y - b));
-              if (maxHeap.size() > k)
-                maxHeap.pop();
+        
+        for(int a = 1; a <= 100; ++a)
+            for(int b = 1; b <= 100; ++b) {
+                priority_queue<int> mh;
+                for(const auto& p : points) {
+                    int x = p[0]; int y = p[1];
+                    mh.push(abs(x - a) + abs(y - b));
+                    if(mh.size() > k) mh.pop();
+                }
+                ans = min(ans, mh.top());
             }
-            ans = min(ans, maxHeap.top());
-          }
-
+        
         return ans;
     }
 };
