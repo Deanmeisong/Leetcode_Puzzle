b9de9f124e466e69473dbb6a6a76053801d3daa6
Deanmeisong <deanmeisong@gmail.com>
2023-10-24 13:12:17 +1100

    Time: 325 ms (59.19%), Space: 124.1 MB (57.40%) - LeetHub

--- 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -13,18 +13,20 @@ public:
         int ret = 0;
         
         int m = lower_bound(pos.begin(), pos.end(), startPos) - pos.begin();
-        int j = 0;
         for(int i = m; i < n; ++i) {
-            while(pos[j] <= startPos && pos[i] - startPos + 2 * (startPos - pos[j]) > k) ++j;
-            if(pos[j] <= startPos) ret = max(ret, presum[i] - (j == 0 ? 0 : presum[j - 1]));
-            else if(pos[i] - startPos <= k) ret = max(ret, presum[i] - (j == 0 ? 0 : presum[j - 1]));
+            if(pos[i] - startPos > k) break;
+            double d = (k - (pos[i] - startPos)) / 2;
+            int j = lower_bound(pos.begin(), pos.end(), startPos - d) - pos.begin();
+            if(j >= 0 && j < n) ret = max(ret, presum[i] - (j == 0 ? 0 : presum[j - 1]));
+
         }
-        j = n - 1;
+        
         m = upper_bound(pos.begin(), pos.end(), startPos) - pos.begin() - 1;
         for(int i = m; i >= 0; --i) {
-            while(pos[j] >= startPos && startPos - pos[i] + 2 * (pos[j] - startPos) > k) --j;
-            if(pos[j] >= startPos) ret = max(ret, presum[j] - (i == 0 ? 0 :presum[i - 1]));
-            else if(startPos - pos[i] <= k) ret = max(ret, presum[j] - (i == 0 ? 0 :presum[i - 1]));
+            if(startPos - pos[i] > k) break;
+            double d = (k - (startPos - pos[i])) / 2;
+            int j = upper_bound(pos.begin(), pos.end(), startPos + d) - pos.begin() - 1;
+            if(j >= 0 && j < n) ret = max(ret, presum[j] - (i == 0 ? 0 : presum[i - 1]));
         }
         
         return ret;
