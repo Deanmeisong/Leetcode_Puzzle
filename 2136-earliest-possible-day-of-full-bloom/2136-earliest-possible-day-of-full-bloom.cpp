fc6f2f245306a3cdd693786303c9b35c1dd1b8be
Deanmeisong <deanmeisong@gmail.com>
2023-10-27 12:42:17 +1100

    Time: 610 ms (5.46%), Space: 202.9 MB (15.84%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -3,15 +3,40 @@ public:
     int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
         int n = plantTime.size();
         vector<vector<int>> time(n);
-        for(int i = 0; i < n; ++i) time[i] = {plantTime[i], growTime[i]};
-        sort(time.begin(), time.end(), [](auto& a, auto& b){return a[1] > b[1];});
+        for(int i = 0; i < n; ++i) time[i] = {plantTime[i], -growTime[i]};
+        sort(time.begin(), time.end(), [](auto& a, auto& b){return a[1] < b[1];});
         
-        int ret = 0, days = 0;
-        for(auto t : time) {
-            days += t[0];
-            ret = max(ret, days + t[1]);
+        int l= 1, r = INT_MAX;
+        while(l < r) {
+            int m = l + (r - l)/2;
+            if(check(m, time)) r = m;
+            else l = m + 1;
         }
         
-        return ret;
+        return l;
+     
+    }
+    
+    bool check(int T, vector<vector<int>>& time) {
+        int n = time.size();
+        int days = 0;
+        
+        for (int i=0; i<n; i++)
+        {
+            days += time[i][0];
+            
+            if (days > T+time[i][1])
+            {
+                return false;
+            }
+        }        
+        return true;      
+//         int days = 0;
+//         for(auto t : time) {
+//             days += t[0];
+//             if(days > T + t[1]) return false;
+//         }
+        
+//         return true;
     }
 };
