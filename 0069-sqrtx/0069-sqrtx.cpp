b8627f388640494c3c2216764ac6b434fc41020a
Deanmeisong <deanmeisong@gmail.com>
2023-11-09 00:18:01 +1100

    Time: 3 ms (38.10%), Space: 6.2 MB (65.14%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 1) return 1;
+        int l = 1, r = x;
+        while(l < r) {
+            int m = (r - l) / 2 + l;
+            if(m > x / m) r = m;
+            else l = m + 1;
+        }
+        return l - 1;
+    }
+};
