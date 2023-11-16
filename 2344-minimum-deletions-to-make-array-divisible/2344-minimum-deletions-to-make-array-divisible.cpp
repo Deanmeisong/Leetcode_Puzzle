f5cb2270be272d106df77e7b74e01a72f5655465
Deanmeisong <deanmeisong@gmail.com>
2023-11-17 10:57:08 +1100

    Time: 114 ms (76.95%), Space: 88.6 MB (35.59%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -3,9 +3,18 @@ public:
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
         sort(nums.begin(), nums.end());
         int x = numsDivide[0];
-        for(int num : numsDivide) x = gcd(x, num);
+        for(int num : numsDivide) x = gcdE(x, num);
         for(int i = 0; i < nums.size(); ++i)
             if(x % nums[i] == 0) return i;
         return -1;
+    }
+    
+    int gcdE(int a, int b) {
+        while(b > 0) {
+            int tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+        return a;
     }
 };
