fbb30cc5dbcd6fa883395bd4a5fbfc6b9b1299b6
Deanmeisong <deanmeisong@gmail.com>
2023-12-05 01:48:16 +1100

    Time: 129 ms (100.00%), Space: 87.2 MB (22.22%) - LeetHub

--- 2613-beautiful-pairs/2613-beautiful-pairs.cpp
+++ 2613-beautiful-pairs/2613-beautiful-pairs.cpp
@@ -1,76 +1,55 @@
 class Solution {
-    const int N = 100005;
-    inline void better (const vector<int> &v, int &x, int y) {
-        if(x < 0) x = y;
-        if(y >= 0 && (v[x] < v[y] || (v[x] == v[y] && y < x))) x = y;
-    }
-
-    int query(const vector<int> &tree, const vector<int> &v, int ind, int left, int right, int x, int y) {
-        if(x <= left && right <= y) return tree[ind];
-        const int mid = (left + right) >> 1;
-        int r = -1;
-        if(x <= mid) {
-            r = query(tree, v, ind << 1, left, mid, x, y);
-        }
-        if(y > mid) {
-            better(v, r, query(tree, v, (ind << 1) | 1, mid + 1, right, x, y));
-        }
-        return r;
-    }
-
-    void update(vector<int> &tree, const vector<int> &v, int ind, int left, int right, int x, int y) {
-        better(v, tree[ind], y);
-        if (left >= x && right <= x) {
-            return;
-        }
-        const int mid = (left + right) >> 1;
-        if (x <= mid) {
-            update(tree, v, ind << 1, left, mid, x, y);
-        } else {
-            update(tree, v, (ind << 1) | 1, mid + 1, right, x, y);
-        } 
-    }
 public:
     vector<int> beautifulPair(vector<int>& nums1, vector<int>& nums2) {
-        const int n = nums1.size();
-        vector<int> p1(n), p2(n), ind(n);
+        int n = nums1.size();
+        unordered_map<long long, vector<int>> pl;
+        for(int i = 0; i < n; ++i)
+            pl[f(nums1[i], nums2[i])].push_back(i);
+        vector<tuple<int, int, int>> points;
         for(int i = 0; i < n; ++i) {
-            p1[i] = nums2[i] + nums1[i];
-            p2[i] = nums2[i] - nums1[i];
-            ind[i] = i;
+            long long z = f(nums1[i], nums2[i]);
+            if(pl[z].size() > 1) return {i, pl[z][1]};
+            points.emplace_back(nums1[i], nums2[i], i);
         }
-        sort(ind.begin(), ind.end(), [&](const int x, const int y) {
-            return nums2[x] < nums2[y];
-        });
-
-        vector<int> tree1(N << 2, -1); vector<int> tree2(N << 2, -1), ans(2, n);
-        int d = INT_MAX;
-
-        for(int i = 0; i < n; ++i) {
-            const int i1 = query(tree1, p1, 1, 0, n, 0, nums1[ind[i]]);
-            if(i1 >= 0) {
-                const int may = p1[ind[i]] - p1[i1];
-                if(may < d) {
-                    d = may;
-                    ans = {min(ind[i], i1), max(ind[i], i1)};
-                } else if(may == d){
-                    ans = min(ans, {min(ind[i], i1), max(ind[i], i1)});
-                }
+        
+        function<tuple<int, int, int>(int, int)> dfs = [&](int l, int r) -> tuple<int, int, int>{
+            if(l >= r) return {1<<30, -1, -1};
+            int m = (l + r) >> 1;
+            int x = get<0>(points[m]);
+            auto t1 = dfs(l, m); auto t2 = dfs(m + 1, r);
+            if (get<0>(t1) > get<0>(t2) || (get<0>(t1) == get<0>(t2) && (get<1>(t1) > get<1>(t2) || (get<1>(t1) == get<1>(t2) && get<2>(t1) > get<2>(t2))))) swap(t1, t2);
+            vector<tuple<int, int, int>> t;
+            for(int i = l; i <= r; ++i) {
+                if(abs(get<0>(points[i]) - x) <= get<0>(t1)) t.emplace_back(points[i]);
             }
-            const int i2 = query(tree2, p2, 1, 0, n, nums1[ind[i]], n);
-            if(i2 >= 0) {
-                const int may = p2[ind[i]] - p2[i2];
-                if(may < d) {
-                    d = may;
-                    ans = {min(ind[i], i2), max(ind[i], i2)};
-                } else if(may == d){
-                    ans = min(ans, {min(ind[i], i2), max(ind[i], i2)});
+            sort(t.begin(), t.end(), [](const tuple<int, int, int>& a, const tuple<int, int, int>& b) {
+                return get<1>(a) < get<1>(b);
+            });
+            
+            for(int i = 0; i < t.size(); ++i)
+                for(int j = i + 1; j < t.size(); ++j) {
+                    if(get<1>(t[j]) - get<1>(t[i]) > get<0>(t1)) break;
+                    int pi = min(get<2>(t[i]), get<2>(t[j]));
+                    int pj = max(get<2>(t[i]), get<2>(t[j]));
+                    int d = dist(get<0>(t[i]), get<1>(t[i]), get<0>(t[j]), get<1>(t[j]));
+                    if (d < get<0>(t1) || (d == get<0>(t1) && (pi < get<1>(t1) || (pi == get<1>(t1) && pj < get<2>(t1))))) {
+                        t1 = {d, pi, pj};
+                    }
+                    
                 }
-            }
-
-            update(tree1, p1, 1, 0, n, nums1[ind[i]], ind[i]);
-            update(tree2, p2, 1, 0, n, nums1[ind[i]], ind[i]);
-        }
-        return ans;
+            return t1;
+        };
+        
+        sort(points.begin(), points.end());
+        auto [_, pi, pj] = dfs(0, points.size() - 1);
+        return {pi, pj};
+    }
+    
+    long long f(int x, int y) {
+        return x * 100000LL + y;
+    }
+    
+    int dist(int x1, int y1, int x2, int y2) {
+        return abs(x1 - x2) + abs(y1 - y2);
     }
 };
