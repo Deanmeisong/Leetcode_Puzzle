1fc8fec9f978d84ee51719f591de26160448b46a
Deanmeisong <deanmeisong@gmail.com>
2023-12-13 22:37:01 +1100

    Time: 12 ms (28.51%), Space: 12.5 MB (94.71%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        long sum1=0, sum2=0, cnt=0;
-        auto arr2 = arr;
-        sort(arr2.begin(), arr2.end());
-        
-        
-        for(int i = 0; i < arr.size(); ++i) {
-            sum1 += arr[i];
-            sum2 += arr2[i];
-            if(sum1 == sum2) {
-                ++cnt;
-                sum1 = 0; sum2 = 0;
+        stack<int> st; int curMax = INT_MIN;
+        for(int x : arr) {
+            if(st.empty() || st.top() <= x) {
+                st.push(x);
+                curMax = x;
+            } else {
+                while(!st.empty() && st.top() > x) {
+                    st.pop();
+                }
+                st.push(curMax);
             }
         }
-        
-        return cnt;
+        return st.size();
     }
 };
