e694fde2b3a1e94606cfcf4cb46295ecab2baf05
Deanmeisong <deanmeisong@gmail.com>
2023-12-18 10:37:33 +1100

    Time: 47 ms (5.39%), Space: 395.6 MB (5.15%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -4,27 +4,7 @@ public:
         int s = 0;
         return helper(expression, s);
     }
-    
-  //   bool parse(const string& exp, int& s) {
-  //       char ch = exp[s++];    
-  //       if (ch == 't') return true;      
-  //       if (ch == 'f') return false;
-  //       if (ch == '!') {
-  //         bool ans = !parse(exp, ++s);
-  //         ++s;
-  //         return ans;
-  //       } 
-  //       bool is_and = (ch == '&');
-  //       bool ans = is_and;
-  //       ++s;
-  //       while (true) {
-  //         if (is_and) ans &= parse(exp, s);
-  //         else ans |= parse(exp, s);
-  //         if (exp[s++] == ')') break;
-  //       }
-  //       return ans;
-  // }
-    
+        
     bool helper(string e, int& s) {
         char ch = e[s++];
         if(ch == 't') return true;
