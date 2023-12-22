a79ce44067fa6830957f036b8de6c6ee2b0e2128
Deanmeisong <deanmeisong@gmail.com>
2023-12-22 18:40:04 +1100

    Time: 171 ms (98.22%), Space: 18.7 MB (10.68%) - LeetHub

--- 1231-divide-chocolate/1231-divide-chocolate.py
+++ 1231-divide-chocolate/1231-divide-chocolate.py
@@ -1,11 +1,6 @@
-class Solution(object):
-    def maximizeSweetness(self, sweetness, k):
-        """
-        :type sweetness: List[int]
-        :type k: int
-        :rtype: int
-        """
-        def ok(S):
+class Solution:
+    def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
+        def ok(S: int):
             cnt, amount = 0, 0
             for x in sweetness:
                 amount += x
@@ -16,7 +11,7 @@ class Solution(object):
         
         l, r = 0, sum(sweetness)
         while l < r:
-            m = (r - l + 1) / 2 + l
+            m = (r - l + 1) // 2 + l
             if ok(m): l = m
             else: r = m - 1
         return l
