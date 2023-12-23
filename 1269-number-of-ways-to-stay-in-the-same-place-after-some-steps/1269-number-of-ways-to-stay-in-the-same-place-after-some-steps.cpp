37b64de9add294a3a4690982f1f6baf72f76dd7c
Deanmeisong <deanmeisong@gmail.com>
2023-12-23 18:25:31 +1100

    Time: 11 ms (82.95%), Space: 13.7 MB (28.37%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int numWays(int steps, int arrLen) {
-        int n = min(steps/2+2, arrLen);
-        vector<long>dp(n);
-        long M = 1e9+7;
+        int n = min(arrLen, steps / 2 + 2);
+        long M = 1e9 + 7;
+        vector<long> dp(n);
         dp[0] = 1;
-        for (int k=0; k<steps; k++)
-        {
-            auto dp2 = dp;
-            for (int i=0; i<n; i++)
-            {
-                dp[i] = dp2[i] + (i>=1 ? dp2[i-1]:0) + (i+1<n ? dp2[i+1] : 0);
+        for(int k = 0; k < steps; ++k) {
+             auto dp2 = dp;
+            for(int i = 0; i < n; ++i) {
+                dp[i] = dp2[i] + (i >= 1 ? dp2[i - 1] : 0) + (i < n - 1 ? dp2[i + 1] : 0);
                 dp[i] %= M;
             }
         }
+  
         return dp[0];
     }
 };
