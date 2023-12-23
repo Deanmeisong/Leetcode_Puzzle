c6f1f05239b3800d43aa2a81d8d139a2746923f2
Deanmeisong <deanmeisong@gmail.com>
2023-12-23 18:36:25 +1100

    Time: 101 ms (90.00%), Space: 13.3 MB (90.00%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -1,16 +1,19 @@
-class Solution {
-    public int numWays(int steps, int arrLen) {
-        int n = Math.min(steps/2+2, arrLen);
-        long[] dp = new long[n];
-        long M = 1000000007;
-        dp[0] = 1;
-        for (int k=0; k<steps; k++) {
-            long[] dp2 = Arrays.copyOf(dp, n);
-            for (int i=0; i<n; i++) {
-                dp[i] = dp2[i] + (i>=1 ? dp2[i-1]:0) + (i+1<n ? dp2[i+1] : 0);
-                dp[i] %= M;
-            }
-        }
-        return (int)dp[0];
-    }
-} 
+class Solution(object):
+    def numWays(self, steps, arrLen):
+        """
+        :type steps: int
+        :type arrLen: int
+        :rtype: int
+        """
+        n = min(steps//2+2, arrLen)
+        dp = [0] * n
+        M = 10**9 + 7
+        dp[0] = 1
+        for k in range(steps):
+            dp2 = dp[:]
+            for i in range(n):
+                dp[i] = dp2[i] + (dp2[i-1] if i>=1 else 0) + (dp2[i+1] if i+1<n else 0)
+                dp[i] %= M
+        return dp[0]
+
+                
