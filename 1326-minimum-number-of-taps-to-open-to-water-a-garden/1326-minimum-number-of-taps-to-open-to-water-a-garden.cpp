ac2f66178fb819a1a0e9d8beabfee685a9e8d9eb
Deanmeisong <deanmeisong@gmail.com>
2023-12-24 20:32:23 +1100

    Time: 11 ms (77.97%), Space: 15.3 MB (49.49%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<pair<int, int>> t;
+        vector<int> nums(ranges.size());
         for(int i = 0; i <= n; ++i) {
-            t.emplace_back(max(0, i - ranges[i]), min(i + ranges[i], n));
+            int s = max(0, i - ranges[i]);
+            nums[s] = max(nums[s], i + ranges[i]);
         }
-        sort(t.begin(), t.end());
-        int e = 0, l = 0, ans = 0, i = 0;
-        while(e < n) {
-            while(i <= n && t[i].first <= l) {
-                e = max(e, t[i++].second);
-            }
-            if(l == e) return -1;
-            l = e;
-            ++ans;
+        int i = 0, e = 0, l = 0, step = 0;
+        for(int i = 0; i <= n; ++i) {
+            if(i > e) return -1;
+            if(i > l) {++step; l = e;}
+            e = max(e, nums[i]);
         }
-        return ans;
-
+        return step;
     }
 };
