f41324fb8589fdefd009853f6bfca91af9f33a3c
Deanmeisong <deanmeisong@gmail.com>
2023-12-27 14:59:38 +1100

    Time: 60 ms (100.00%), Space: 18.2 MB (50.00%) - LeetHub

---
+++ 1516-move-sub-tree-of-n-ary-tree/1516-move-sub-tree-of-n-ary-tree.py
@@ -0,0 +1,33 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def moveSubTree(self, root: 'Node', p: 'Node', q: 'Node') -> 'Node':
+        if p in q.children: return root
+        
+        dummy = Node()
+        dummy.children.append(root)
+        p_parent = self.dfs_parent(dummy, p)
+        q_in_p = self.dfs_parent(p, q)
+        
+        p_index = p_parent.children.index(p)
+        p_parent.children.pop(p_index)
+        q.children.append(p)
+        
+        if q_in_p:
+            q_in_p.children.remove(q)
+            p_parent.children.insert(p_index, q)
+        
+        return dummy.children[0]
+        
+    def dfs_parent(self, node: 'Node', tar: 'Node'):
+        if tar in node.children: return node
+        for x in node.children:
+            ans = self.dfs_parent(x, tar)
+            if ans: return ans
+        return None
