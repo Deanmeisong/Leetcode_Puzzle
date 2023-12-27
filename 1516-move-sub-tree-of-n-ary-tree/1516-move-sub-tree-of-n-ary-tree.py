8abf53390bec368c8a255837e09910447fdfc82e
Deanmeisong <deanmeisong@gmail.com>
2023-12-27 15:11:45 +1100

    Time: 54 ms (100.00%), Space: 14.9 MB (100.00%) - LeetHub

--- 1516-move-sub-tree-of-n-ary-tree/1516-move-sub-tree-of-n-ary-tree.py
+++ 1516-move-sub-tree-of-n-ary-tree/1516-move-sub-tree-of-n-ary-tree.py
@@ -1,33 +1,42 @@
 """
 # Definition for a Node.
-class Node:
+class Node(object):
     def __init__(self, val=None, children=None):
         self.val = val
         self.children = children if children is not None else []
 """
 
-class Solution:
-    def moveSubTree(self, root: 'Node', p: 'Node', q: 'Node') -> 'Node':
-        if p in q.children: return root
+class Solution(object):
+    def moveSubTree(self, root, p, q):
+        """
+        :type root: Node
+        :type p: Node
+        :type q: Node
+        :rtype: Node
+        """
         
-        dummy = Node()
-        dummy.children.append(root)
-        p_parent = self.dfs_parent(dummy, p)
+        if p in q.children: return root
+        d = Node()
+        d.children.append(root)
+        
+        pp = self.dfs_parent(d, p)
         q_in_p = self.dfs_parent(p, q)
         
-        p_index = p_parent.children.index(p)
-        p_parent.children.pop(p_index)
-        q.children.append(p)
+        pi = pp.children.index(p)
+        pp.children.pop(pi)
         
+        q.children.append(p)
+    
         if q_in_p:
             q_in_p.children.remove(q)
-            p_parent.children.insert(p_index, q)
+            pp.children.insert(pi, q)
         
-        return dummy.children[0]
-        
-    def dfs_parent(self, node: 'Node', tar: 'Node'):
-        if tar in node.children: return node
-        for x in node.children:
+        return d.children[0]
+    
+    def dfs_parent(self, n, tar):
+        if tar in n.children: return n
+        for x in n.children:
             ans = self.dfs_parent(x, tar)
             if ans: return ans
         return None
+        
