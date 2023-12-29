631214e3486adf407f7f02df77837b9f4f925c33
Deanmeisong <deanmeisong@gmail.com>
2023-12-29 23:55:21 +1100

    Time: 348 ms (12.83%), Space: 17.5 MB (47.28%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        n = len(s1)
+        dp = [[[False] * (n + 1) for _ in range(n)] for _ in range(n + 1)]
+        for k in range(1, n + 1):
+            for i in range(n - k + 1):
+                for j in range(n - k + 1):
+                    if k == 1:
+                        dp[i][j][k] = s1[i] == s2[j]
+                        continue
+                    for u in range(1, k):
+                        o1 = dp[i][j][u] and dp[i + u][j + u][k - u]
+                        o2 = dp[i][j + k - u][u] and dp[i + u][j][k - u]
+                        if o1 or o2:
+                            dp[i][j][k] = True
+                            break
+        return dp[0][0][n]
