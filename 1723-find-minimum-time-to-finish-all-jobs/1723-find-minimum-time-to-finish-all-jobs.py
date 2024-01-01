27f4b55b0f74dc13ae0ff61235dd0db75ca3cde5
Deanmeisong <deanmeisong@gmail.com>
2024-01-01 23:48:01 +1100

    Time: 3269 ms (29.41%), Space: 13.3 MB (41.18%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.py
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.py
@@ -1,20 +1,26 @@
-class Solution:
-    def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
-        works = [0] * k
+class Solution(object):
+    def minimumTimeRequired(self, jobs, k):
+        """
+        :type jobs: List[int]
+        :type k: int
+        :rtype: int
+        """
         self.res = sys.maxsize
+        count = [0] * k
+        
         def dfs(cur):
-            if cur == len(jobs):
-                self.res = min(self.res, max(works))
+            if cur == len(jobs): 
+                self.res = min(self.res, max(count))
                 return
+            
             seen = set()
-            for x in range(k):
-                if works[x] in seen: continue
-                if works[x] + jobs[cur] >= self.res: continue
-                seen.add(works[x])
-                works[x] += jobs[cur]
+            for i in range(k):
+                if count[i] in seen: continue
+                if count[i] + jobs[cur] >= self.res: continue
+                seen.add(count[i])
+                count[i] += jobs[cur]
                 dfs(cur + 1)
-                works[x] -= jobs[cur]
+                count[i] -= jobs[cur]
         
         dfs(0)
         return self.res
-                
