787ea602978f7dae9718f169c4a7612e20718192
Deanmeisong <deanmeisong@gmail.com>
2024-01-03 21:58:18 +1100

    Time: 136 ms (87.56%), Space: 24.5 MB (40.44%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -1,15 +1,17 @@
 class Solution {
+    int dp[2000][2000];
 public:
     bool checkPartitioning(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, 1));
-        for(int len = 2; len <= n; ++len)
-            for(int i = 0, j = i + len - 1; j < n; ++i, ++j)
-                dp[i][j] = s[i] == s[j] && dp[i + 1][j - 1];
+        for(int i = 0; i < n; ++i) dp[i][i] = 1;
+        for(int i = 0; i + 1 < n; ++i) dp[i][i + 1] = (s[i] == s[i + 1]);
+        for(int len = 3; len <= n; ++len)
+            for(int i = 0; i + len - 1 < n; ++i)
+                dp[i][i + len - 1] = (s[i] == s[i + len - 1] && dp[i + 1][i + len - 2]);
+        
         for(int i = 1; i < n; ++i)
             for(int j = i; j + 1 < n; ++j)
                 if(dp[0][i - 1] && dp[i][j] && dp[j + 1][n - 1]) return true;
         return false;
-
     }
 };
