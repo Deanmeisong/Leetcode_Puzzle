7cfbf1ba0e1185d34a554482c5bd80e236a6a6c9
Deanmeisong <deanmeisong@gmail.com>
2024-01-05 23:54:50 +1100

    Time: 32 ms (85.71%), Space: 17.1 MB (27.38%) - LeetHub

--- 1808-maximize-number-of-nice-divisors/1808-maximize-number-of-nice-divisors.py
+++ 1808-maximize-number-of-nice-divisors/1808-maximize-number-of-nice-divisors.py
@@ -1,15 +1,7 @@
-class Solution(object):
-    def maxNiceDivisors(self, x):
-        """
-        :type primeFactors: int
-        :rtype: int
-        """
-        if x == 1: return 1
-        if x == 2: return 2
-        if x == 3: return 3
-        
-        m = 10**9 + 7
-        a, b = x / 3, x % 3
-        if b == 0: return pow(3, a, m);
-        if b == 1: return pow(3, a - 1, m) * 4 % m
-        if b == 2: return pow(3, a, m) * 2 % m
+class Solution:
+    def maxNiceDivisors(self, n: int) -> int:
+        M = 10**9 + 7
+        if n <= 3: return n
+        if n % 3 == 0: return pow(3, n//3, M)
+        if n % 3 == 1: return (pow(3, (n-4)//3, M) * 4) % M
+        return (2*pow(3, n//3, M)) % M
