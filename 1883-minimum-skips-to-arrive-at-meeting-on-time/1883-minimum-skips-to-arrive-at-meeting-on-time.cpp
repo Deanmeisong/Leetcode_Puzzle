334dae1f8a526760ba6b466ba2f35a9edb4dbe9e
Deanmeisong <deanmeisong@gmail.com>
2024-01-08 01:20:17 +1100

    Time: 227 ms (21.05%), Space: 9.4 MB (64.47%) - LeetHub

---
+++ 1883-minimum-skips-to-arrive-at-meeting-on-time/1883-minimum-skips-to-arrive-at-meeting-on-time.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSkips(vector<int>& dist, int speed, int hoursBefore) {
+        int n = dist.size();
+        vector<int> dp(n + 1, 0);
+        
+        for(int i = 0; i < n; ++i)
+            for(int j = n; j >= 0; --j) {
+                dp[j] += dist[i];
+                if(i < n - 1) dp[j] = (dp[j] + speed - 1) / speed * speed;
+                if(j > 0) dp[j] = min(dp[j], (dp[j - 1] + dist[i]));
+            }
+        
+        for(int i = 0; i < n; ++i)
+            if(dp[i] <= (long)hoursBefore * speed) return i;
+        return -1;
+    }
+};
