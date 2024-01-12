c5cfd5bd78f307a4c5a3f11d2ce9825a7a0bcc8a
Deanmeisong <deanmeisong@gmail.com>
2024-01-12 17:57:58 +1100

    Time: 137 ms (96.63%), Space: 52.8 MB (95.51%) - LeetHub

--- 1998-gcd-sort-of-an-array/1998-gcd-sort-of-an-array.cpp
+++ 1998-gcd-sort-of-an-array/1998-gcd-sort-of-an-array.cpp
@@ -15,45 +15,24 @@ public:
     }
     
     vector<int> getPrimes(int n) {
-                auto q=vector<int>(n+1,0);
-        
-        for (int i=2; i<=sqrt(n); i++)
-        {
-            if (q[i]==0)
-            {
-                int j=i*2;
-                while (j<n)
-                {
-                    q[j]=1;
-                    j+=i;
+        auto q=vector<int>(n+1,0);
+
+        for(int x = 2; x <= sqrt(n); ++x) {
+            if(q[x] == 0) {
+                int y = 2 * x;
+                while(y < n) {            
+                    q[y] = 1;
+                    y += x;
                 }
             }
         }
-        vector<int>primes;
-        for (int i=2; i<=n; i++)
-        {
-            if (q[i]==0)
-                primes.push_back(i);                
-        }
+        
+        vector<int> primes;
+        for(int x = 2; x <= n; ++x)
+            if(q[x] == 0) primes.push_back(x);
+        
         return primes;
-//         auto q = vector<int>(n + 1, 0);
         
-//         for(int x = 2; x <= sqrt(n); ++x) {
-//             if(q[x] == 0) {
-//                 int y = 2 * x;
-//                 q[y] = 1;
-//                 while(y < n) {
-//                     y += x;
-//                     q[y] = 1;
-//                 }
-//             }
-//         }
-        
-//         vector<int> primes;
-//         for(int x = 2; x <= n; ++x)
-//             if(q[x] == 0) primes.push_back(x);
-        
-//         return primes;
     }
     
     bool gcdSort(vector<int>& nums) {
