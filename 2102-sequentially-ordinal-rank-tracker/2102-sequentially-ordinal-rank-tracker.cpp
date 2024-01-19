46ab1ca54b5a28e790e87237b9651d101471f488
Deanmeisong <deanmeisong@gmail.com>
2024-01-19 20:21:43 +1100

    Time: 353 ms (71.11%), Space: 149.5 MB (50.00%) - LeetHub

---
+++ 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.cpp
@@ -0,0 +1,27 @@
+class SORTracker {
+public:
+    set<pair<int, string>> Set;
+    set<pair<int, string>> :: iterator iter;
+    
+    SORTracker() {
+        Set.insert({INT_MIN, "###"});
+        iter = Set.begin();
+    }
+    
+    void add(string name, int score) {
+        Set.insert({-score, name});
+        if(make_pair(-score, name) < *iter) iter = prev(iter);
+    }
+    
+    string get() {
+        iter = next(iter);
+        return (*iter).second;
+    }
+};
+
+/**
+ * Your SORTracker object will be instantiated and called as such:
+ * SORTracker* obj = new SORTracker();
+ * obj->add(name,score);
+ * string param_2 = obj->get();
+ */
