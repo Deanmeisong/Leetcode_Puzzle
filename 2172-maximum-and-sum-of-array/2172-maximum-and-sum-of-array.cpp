ffd7d898868a03a0d74ea1c7e773d294ab9ec65a
Deanmeisong <deanmeisong@gmail.com>
2024-01-21 13:31:04 +1100

    Time: 538 ms (23.92%), Space: 25.9 MB (45.65%) - LeetHub

---
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumANDSum(vector<int>& nums, int numSlots) {
+        int n = nums.size(), m = numSlots;
+        nums.insert(nums.begin(), 0);
+        vector<vector<int>> dp(n + 1, vector<int>(pow(3, m), INT_MIN/2));
+        dp[0][0] = 0;
+        int ret = 0;
+        
+        for(int i = 1; i <= n; ++i) {
+            for(int state = 0; state < pow(3, m); ++state) {
+                for(int j = 0; j < m; ++j) {
+                    if(check(state, j) >= 1) dp[i][state] = max(dp[i][state], dp[i - 1][state - pow(3, j)] + (nums[i] & (j + 1)));
+                }
+                if(i == n) ret = max(ret, dp[i][state]);            
+            }  
+        }
+        
+        return ret;
+
+    }
+    
+    int check(int state, int j) {
+        for(int i = 0; i < j; ++i) state /= 3;
+        return state % 3;
+    }
+    
+    
+    
+};
