66a32d5570f698d7ac93bd9f693e0cad9df9e30a
Deanmeisong <deanmeisong@gmail.com>
2024-01-23 01:07:06 +1100

    Time: 1306 ms (100.00%), Space: 28.3 MB (100.00%) - LeetHub

---
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def replaceNonCoprimes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        res = []
+        
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+        
+        def lcm(x, y):
+            return (x * y) / gcd(x, y)
+        
+        for x in nums:
+            while res and gcd(res[-1], x) > 1:
+                x = lcm(res[-1], x)
+                res.pop()
+            res.append(x)
+        return res
