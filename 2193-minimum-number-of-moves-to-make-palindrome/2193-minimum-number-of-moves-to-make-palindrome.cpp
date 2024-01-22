6911ee0baf1b1bdac9a710012e66d5479b3360e0
Deanmeisong <deanmeisong@gmail.com>
2024-01-22 21:13:56 +1100

    Time: 7 ms (96.62%), Space: 8 MB (12.41%) - LeetHub

---
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minMovesToMakePalindrome(string s) {
+        int ret = 0, n = s.length(), count = 0;
+        
+        for(int i = 0; i < n / 2; ++i) {
+            int j = n - 1 - count;
+            while(s[i] != s[j]) --j;
+            if(i == j) ret += n/2 - i;
+            else {
+                int k = n - count - 1 - j;
+                ret += k;
+                ++count;
+                while(k--) swap(s[j], s[j + 1]), ++j;
+            }
+        }
+        
+        return ret;
+    }
+};
