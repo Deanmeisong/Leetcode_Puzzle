616cf54a4a477ee91c08dd013ebfebea4777138d
Deanmeisong <deanmeisong@gmail.com>
2024-01-25 21:48:36 +1100

    Time: 25 ms (52.78%), Space: 15.3 MB (36.11%) - LeetHub

--- 2247-maximum-cost-of-trip-with-k-highways/2247-maximum-cost-of-trip-with-k-highways.cpp
+++ 2247-maximum-cost-of-trip-with-k-highways/2247-maximum-cost-of-trip-with-k-highways.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     int dp[16][65536] = {};
-    int dfs(int i, vector<vector<array<int, 2>>> &al, int mask, int k) {
-        if (k == 0)
-            return 0;
-        if (dp[i][mask] == 0) {
-            dp[i][mask] = INT_MIN;
-            for (auto [j, cost] : al[i])
-                if (((1 << j) & mask) == 0)
-                    dp[i][mask] = max(dp[i][mask], cost + dfs(j, al, mask + (1 << j), k - 1));
-        }
-        return dp[i][mask];
-    }
     int maximumCost(int n, vector<vector<int>>& highways, int k) {
-        if (k > n - 1)
-        return -1;
-        vector<vector<array<int, 2>>> al(n);
-        for (auto &h : highways) {
-            al[h[0]].push_back({h[1], h[2]});
-            al[h[1]].push_back({h[0], h[2]});
+        if(k > n - 1) return -1;
+        int ans = INT_MIN;
+        
+        vector<vector<array<int, 2>>> adj(n);
+        
+        for(const auto& h : highways) {
+            adj[h[0]].push_back({h[1], h[2]});
+            adj[h[1]].push_back({h[0], h[2]});
         }
-        int res = INT_MIN;
-        for (int i = 0; i < n; ++i)
-            res = max(res, dfs(i, al, (1 << i), k));
-        return res < 0 ? -1 : res;
+        
+        function<int(int, int, int)> dfs = [&](int i, int mask, int k1)->int {
+            if(k1 == 0) return 0;
+            if(dp[i][mask] == 0) {
+                dp[i][mask] = INT_MIN;
+                for(auto [j, cost] : adj[i])
+                    if(((1<<j) & mask) == 0) dp[i][mask] = max(dp[i][mask], cost + dfs(j, mask + (1<<j), k1 - 1));
+            }
+            return dp[i][mask];
+        };
+        
+        for(int i = 0; i < n; ++i)
+            ans = max(ans, dfs(i, (1<<i), k));
+        
+        return ans < 0 ? -1 : ans;
     }
+
 };
