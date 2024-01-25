840734e5246f1b44729534af2a761323f12f52d0
Deanmeisong <deanmeisong@gmail.com>
2024-01-25 22:02:39 +1100

    Time: 0 ms (100.00%), Space: 11.7 MB (66.67%) - LeetHub

--- 2247-maximum-cost-of-trip-with-k-highways/2247-maximum-cost-of-trip-with-k-highways.cpp
+++ 2247-maximum-cost-of-trip-with-k-highways/2247-maximum-cost-of-trip-with-k-highways.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    int dp[16][65536] = {};
     int maximumCost(int n, vector<vector<int>>& highways, int k) {
-        if(k > n - 1) return -1;
-        int ans = INT_MIN;
-        
+        vector<bool> visited(n, false);
+        priority_queue<tuple<int, int, pair<int, int>>> pq; // cost, node, kth, parent
         vector<vector<array<int, 2>>> adj(n);
-        
         for(const auto& h : highways) {
             adj[h[0]].push_back({h[1], h[2]});
             adj[h[1]].push_back({h[0], h[2]});
         }
         
-        function<int(int, int, int)> dfs = [&](int i, int mask, int k1)->int {
-            if(k1 == 0) return 0;
-            if(dp[i][mask] == 0) {
-                dp[i][mask] = INT_MIN;
-                for(auto [j, cost] : adj[i])
-                    if(((1<<j) & mask) == 0) dp[i][mask] = max(dp[i][mask], cost + dfs(j, mask + (1<<j), k1 - 1));
+        int ret = -1;
+        
+        for(int node = 0; node < n; ++node) {
+            fill(visited.begin(), visited.end(), false);
+            pq.push(make_tuple(0, node, make_pair(0, -1)));
+            while(!pq.empty()) {
+                tuple<int, int, pair<int, int>> t = pq.top(); pq.pop();
+                int cur = get<1>(t);
+                int cost = get<0>(t);
+                int kth = get<2>(t).first;
+                int parent = get<2>(t).second;
+                visited[cur] = true;
+                
+                if(kth == k) {
+                    ret = max(ret, cost);
+                    break;
+                }
+                
+                for(auto [nxt, cost2] : adj[cur]) 
+                    if(visited[nxt] == false) pq.push(make_tuple(cost + cost2, nxt, make_pair(kth + 1, cur)));
+                
             }
-            return dp[i][mask];
-        };
+            
+            while(!pq.empty()) {
+                pq.pop();
+            }
+        }
         
-        for(int i = 0; i < n; ++i)
-            ans = max(ans, dfs(i, (1<<i), k));
-        
-        return ans < 0 ? -1 : ans;
+        return ret < 0 ? -1 : ret;
     }
-
 };
