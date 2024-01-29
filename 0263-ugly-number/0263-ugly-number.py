8ea4b939771c95e7192006c8956d8d43f4fa97fc
Deanmeisong <deanmeisong@gmail.com>
2024-01-29 22:49:54 +1100

    Time: 35 ms (75.98%), Space: 16.5 MB (67.28%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def isUgly(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
+class Solution:
+    def isUgly(self, n: int) -> bool:
         if n == 0: return False
         ps = {2, 3, 5}
         for p in ps:
