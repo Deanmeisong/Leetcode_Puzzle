d648ea2d5a0d02686681194c5abb741d341a34ea
Deanmeisong <deanmeisong@gmail.com>
2024-02-02 19:01:54 +1100

    Time: 269 ms (29.24%), Space: 78.3 MB (17.64%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -1,34 +1,33 @@
+using AI3 = array<int, 3>;
 using LL = long long;
 LL M = 1e9 + 7;
 class Solution {
 public:
-    int dp[1000][1000];
-    vector<vector<int>> grid;
-    int m; int n;
-    
     int countPaths(vector<vector<int>>& grid) {
-        this->grid = grid;
-        this->m = grid.size(); this->n = grid[0].size();
+        int m = grid.size(); int n = grid[0].size();
+        vector<AI3> arr;
+        for(int i = 0; i < m; ++i)
+            for(int j = 0; j < n; ++j)
+                arr.push_back({grid[i][j], i, j});
+        
+        sort(arr.begin(), arr.end());
+        vector<vector<LL>> dp(m, vector<LL>(n));
         LL ret = 0;
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j) {
-               ret = (ret + dfs(i, j)) % M;
+        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for(auto[value, i, j] : arr) {
+            LL sum = 0;
+            for(int k = 0; k < 4; ++k) {
+                int x = i + dirs[k].first;
+                int y = j + dirs[k].second;
+                if(x < 0 || x >= m || y < 0 || y >= n) continue;
+                if(grid[x][y] >= grid[i][j]) continue;
+                sum = (sum + dp[x][y]) % M;
             }
+            sum += 1;
+            dp[i][j] = sum;
+            ret = (ret + sum) % M;
         }
-        return ret;
-    }
     
-    int dfs(int i, int j) {
-        if(dp[i][j] != 0) return dp[i][j];
-        vector<pair<int, int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        dp[i][j] = 1;
-        for(int k = 0; k < 4; ++k) {
-            int x = dirs[k].first + i; int y = dirs[k].second + j;
-            if(x < 0 || x >= m || y < 0 || y >= n) continue;
-            if(grid[x][y] >= grid[i][j]) continue;
-            dp[i][j] += dfs(x, y);
-            dp[i][j] %= M;
-        }
-        return dp[i][j];
+        return ret;
     }
 };
