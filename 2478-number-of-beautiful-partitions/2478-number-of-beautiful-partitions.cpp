590b2b3c84ccdc2a29db0c0096d2f942c3e4b9f5
Deanmeisong <deanmeisong@gmail.com>
2024-02-07 22:33:10 +1100

    Time: 2254 ms (7.14%), Space: 16.7 MB (65.87%) - LeetHub

--- 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.cpp
+++ 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.cpp
@@ -3,25 +3,38 @@ class Solution {
 public:
     LL dp[1005][1005];
     LL M = 1e9 + 7;
-    int beautifulPartitions(string s, int k, int minLength) {
+    int beautifulPartitions(string s, int K, int minLength) {
         int n = s.length();
         // s.insert(s.begin(), '#');
         s = "#" + s;
         dp[0][0] = 1;
         
-        for(int j = 1; j <= k; ++j) {
-            LL presum = 0;
-            for(int i = 1; i <= n; ++i) {
-                if(i - minLength + 1 >= 1 && isPrime(s[i - minLength + 1]) && !isPrime(s[i - minLength])) {
-                    presum += dp[i - minLength][j - 1];
-                    presum %= M;
-                }
+//         for(int j = 1; j <= k; ++j) {
+//             LL presum = 0;
+//             for(int i = 1; i <= n; ++i) {
+//                 if(i - minLength + 1 >= 1 && isPrime(s[i - minLength + 1]) && !isPrime(s[i - minLength])) {
+//                     presum += dp[i - minLength][j - 1];
+//                     presum %= M;
+//                 }
                 
-                if(!isPrime(s[i])) dp[i][j] = presum;
-            }
-        }
+//                 if(!isPrime(s[i])) dp[i][j] = presum;
+//             }
+//         }
         
-        return dp[n][k];
+            for (int i=1; i<=n; i++)
+                for (int j=1; j<=K; j++)
+                {
+                    if (isPrime(s[i])) {
+                        continue;
+                    }
+                    for (int k=j; (k+minLength-1)<=i; k++)
+                    {
+                        if (isPrime(s[k]))
+                            dp[i][j] = (dp[i][j] + dp[k-1][j-1]) % M;
+                    }
+                }
+        
+        return dp[n][K];
         
     }
     
