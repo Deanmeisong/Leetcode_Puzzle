2ffb14b8ae41564806866be2624f000110c226db
Deanmeisong <deanmeisong@gmail.com>
2024-02-11 12:36:23 +1100

    Time: 197 ms (100.00%), Space: 225.3 MB (100.00%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.rb
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.rb
@@ -4,21 +4,16 @@
 def put_marbles(weights, k)
     n = weights.size
     arr = []
-
-    (0...n - 1).each do |i|
-      arr.push(weights[i] + weights[i + 1])
+    (0 ... n - 1).each do |i|
+        arr.push(weights[i] + weights[i + 1])
     end
-
     arr.sort!
     ret = 0
-
-    (0...k - 1).each do |i|
-      ret += arr[arr.size - 1 - i]
+    (0 ... k - 1).each do |i|
+        ret += arr[arr.size - 1 - i]
     end
-
-    (0...k - 1).each do |i|
-      ret -= arr[i]
+    (0 ... k - 1).each do |i|
+        ret -= arr[i]
     end
-
-    return ret
+    ret
 end
