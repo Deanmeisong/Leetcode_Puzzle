dd3bcc2ccd86b761e9823189a1560ebc4000ad6b
Deanmeisong <deanmeisong@gmail.com>
2024-02-11 12:33:23 +1100

    Time: 197 ms (100.00%), Space: 225.2 MB (100.00%) - LeetHub

---
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.rb
@@ -0,0 +1,24 @@
+# @param {Integer[]} weights
+# @param {Integer} k
+# @return {Integer}
+def put_marbles(weights, k)
+    n = weights.size
+    arr = []
+
+    (0...n - 1).each do |i|
+      arr.push(weights[i] + weights[i + 1])
+    end
+
+    arr.sort!
+    ret = 0
+
+    (0...k - 1).each do |i|
+      ret += arr[arr.size - 1 - i]
+    end
+
+    (0...k - 1).each do |i|
+      ret -= arr[i]
+    end
+
+    return ret
+end
