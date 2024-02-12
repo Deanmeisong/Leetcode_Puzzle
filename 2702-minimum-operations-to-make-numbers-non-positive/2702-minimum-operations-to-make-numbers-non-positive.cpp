c760d542b4cfbb1f655e614841ef90979adff10b
Deanmeisong <deanmeisong@gmail.com>
2024-02-12 22:37:22 +1100

    Time: 165 ms (30.77%), Space: 77.7 MB (19.23%) - LeetHub

--- 2702-minimum-operations-to-make-numbers-non-positive/2702-minimum-operations-to-make-numbers-non-positive.cpp
+++ 2702-minimum-operations-to-make-numbers-non-positive/2702-minimum-operations-to-make-numbers-non-positive.cpp
@@ -3,21 +3,22 @@ public:
     int minOperations(vector<int>& nums, int x, int y) {
         sort(nums.begin(), nums.end());
         int l = 0, r = INT_MAX/2;
+        function<bool(int)> isOK = [&](int k) {
+            int count = 0;
+            for(int i = 0; i < nums.size(); ++i) {
+                if(nums[i] < 1.0 * y * k) continue;
+                count += ceil((nums[i] - y * k) * 1.0 / (x - y));
+                if(count > k) return false;
+            }
+            return true;
+        };
+        
         while(l < r) {
             int m = l + (r - l)/2;
-            if(isOK(nums, m, x, y)) r = m;
+            if(isOK(m)) r = m;
             else l = m + 1;
         }
         return l;
     }
     
-    bool isOK(vector<int>& nums, int k, int x, int y) {
-        int count = 0;
-        for(int i = 0; i < nums.size(); ++i) {
-            if(nums[i] < 1.0 * y * k) continue;
-            count += ceil((nums[i] - y * k) * 1.0 / (x - y));
-            if(count > k) return false;
-        }
-        return true;
-    }
 };
