e7472495c76a0861f6620fc0acc56013415be4c9
Deanmeisong <deanmeisong@gmail.com>
2024-02-18 17:53:11 +1100

    Time: 14 ms (55.79%), Space: 11.6 MB (85.26%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        mask = ~0
+        while mask & num: mask <<= 1
+        return ~num ^ mask
