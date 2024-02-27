25433a75d0f2ee06f449443503deda6b76fc49e6
Deanmeisong <deanmeisong@gmail.com>
2024-02-27 11:25:02 +1100

    Time: 686 ms (62.50%), Space: 271.4 MB (37.50%) - LeetHub

---
+++ 2941-maximum-gcd-sum-of-a-subarray/2941-maximum-gcd-sum-of-a-subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long maxGcdSum(vector<int>& nums, int k) {
+        int n = nums.size();
+        long long s[n+1];
+        s[0] = 0;
+        for(int i = 1; i <= n; ++i) s[i] = s[i-1] + nums[i-1];
+        vector<pair<int, int>> f;
+        long long ans = 0;
+        for(int i = 0; i < n; ++i) {
+            vector<pair<int, int>> g;
+            for(auto [j, v] : f) {
+                int x = gcd(v, nums[i]);
+                if(g.empty() || g.back().second != x) g.push_back({j, x});
+            }
+            f = move(g);
+            f.push_back({i, nums[i]});
+            for(auto [j, v] : f)
+                if(i - j + 1 >= k) ans = max(ans, (s[i + 1] - s[j])*v);
+        }
+        return ans;
+    }
+};
