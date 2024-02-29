d70358438cadaa4fe46e7a4e41200b758273ce61
Deanmeisong <deanmeisong@gmail.com>
2024-02-29 11:52:46 +1100

    Time: 156 ms (92.59%), Space: 117.6 MB (51.85%) - LeetHub

---
+++ 2969-minimum-number-of-coins-for-fruits-ii/2969-minimum-number-of-coins-for-fruits-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumCoins(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> dp(n);
+        dp[0] = prices[0];
+        deque<int> dq;
+        dq.push_back(0);
+        
+        for(int i = 1; i < n; ++i) {
+            dp[i] = dp[dq.front()] + prices[i];
+            if(!dq.empty() && dq.front() + dq.front() + 1 < i) dq.pop_front();
+            while(!dq.empty() && dp[dq.back()] >= dp[i]) dq.pop_back();
+            dq.push_back(i);
+        }
+        
+        return dp[dq.front()];
+    }
+};
