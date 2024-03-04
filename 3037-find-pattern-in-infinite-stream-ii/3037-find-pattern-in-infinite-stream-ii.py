a0e2c1506b3e8b2ea8fe12aec6ed45ede70389d8
Deanmeisong <deanmeisong@gmail.com>
2024-03-04 23:00:53 +1100

    Time: 2629 ms (13.86%), Space: 32.6 MB (92.08%) - LeetHub

---
+++ 3037-find-pattern-in-infinite-stream-ii/3037-find-pattern-in-infinite-stream-ii.py
@@ -0,0 +1,17 @@
+# Definition for an infinite stream.
+# class InfiniteStream:
+#     def next(self) -> int:
+#         pass
+class Solution:
+    def findPattern(self, stream: Optional['InfiniteStream'], pattern: List[int]) -> int:
+        num, target, count, n = 0, 0, 0, len(pattern)
+        mask = (1<<(n-1)) - 1
+        
+        for b in pattern:
+            target = (target << 1) + b
+        
+        while num != target or count < n:
+            num = ((num&mask)<<1) + stream.next();
+            count += 1
+        
+        return count - n
