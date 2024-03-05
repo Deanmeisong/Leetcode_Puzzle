4cedef54fc71fbc436c2e6bc8f279e5b7a0b1a4b
Deanmeisong <deanmeisong@gmail.com>
2024-03-05 21:52:08 +1100

    Time: 1033 ms (17.58%), Space: 121.2 MB (88.81%) - LeetHub

--- 3072-distribute-elements-into-two-arrays-ii/3072-distribute-elements-into-two-arrays-ii.cpp
+++ 3072-distribute-elements-into-two-arrays-ii/3072-distribute-elements-into-two-arrays-ii.cpp
@@ -1,50 +1,40 @@
 class Solution {
 public:
-    int greaterCount(vector<int>& arr, int val) {
-        return arr.end() - upper_bound(arr.begin(), arr.end(), val);
+    int greaterCount(vector<int>& arr, int x) {
+        return arr.end() - upper_bound(arr.begin(), arr.end(), x);
     }
-
     vector<int> resultArray(vector<int>& nums) {
-        int n = nums.size();
-        
-        vector<int> left, right, ll, rr;
-        left.push_back(nums[0]);
-        right.push_back(nums[1]);
-        ll.push_back(nums[0]);
-        rr.push_back(nums[1]);
-        
-        int l = 0, r = 0;
-        
-        for (int i = 2; i < n; i++) {
-            int x = greaterCount(left, nums[i]);
-            int y = greaterCount(right, nums[i]);
-            
-            if (x > y) {
-                
-                left.insert(left.end() - x , nums[i]);
+        vector<int> left, ll, right, rr;
+        int ln = 1, rn = 1;
+        left.push_back(nums[0]); right.push_back(nums[1]);
+        ll.push_back(nums[0]); rr.push_back(nums[1]);
+        for(int i = 2; i < nums.size(); ++i) {
+            int n1 = greaterCount(left, nums[i]);
+            int n2 = greaterCount(right, nums[i]);
+            if(n1 > n2) {
+                left.insert(left.end() - n1, nums[i]);
                 ll.push_back(nums[i]);
-                l++;
-            } else if (x < y) {
-                
-                right.insert(right.end() - y, nums[i]);
+                ++ln;
+            } else if(n1 < n2) {
+                right.insert(right.end() - n2, nums[i]);
                 rr.push_back(nums[i]);
-                r++;
+                ++rn;
             } else {
-                if (l <= r) {
-                    
-                    left.insert(left.end() - x, nums[i]);
+                if(ln < rn) {
+                    left.insert(left.end() - n1, nums[i]);
                     ll.push_back(nums[i]);
-                    l++;
-                } else {
-                    
-                    right.insert(right.end() - y, nums[i]);
+                    ++ln;
+                } else if(ln > rn) {
+                    right.insert(right.end() - n2, nums[i]);
                     rr.push_back(nums[i]);
-                    r++;
+                    ++rn;
+                } else {
+                    left.insert(left.end() - n1, nums[i]);
+                    ll.push_back(nums[i]);
+                    ++ln;
                 }
             }
         }
-        
-        
         ll.insert(ll.end(), rr.begin(), rr.end());
         return ll;
     }
