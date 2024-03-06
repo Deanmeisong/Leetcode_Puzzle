32300308a5fea9bba610643903046859a2a4c58c
Deanmeisong <deanmeisong@gmail.com>
2024-03-06 22:30:07 +1100

    Time: 250 ms (66.67%), Space: 162.9 MB (83.33%) - LeetHub

--- 2814-minimum-time-takes-to-reach-destination-without-drowning/2814-minimum-time-takes-to-reach-destination-without-drowning.cpp
+++ 2814-minimum-time-takes-to-reach-destination-without-drowning/2814-minimum-time-takes-to-reach-destination-without-drowning.cpp
@@ -38,48 +38,10 @@ public:
             }
         }
         
-        
-        //  int m = land.size(), n = land[0].size();
-        // bool vis[m][n];
-        // int g[m][n];
-        // memset(vis, false, sizeof(vis));
-        // memset(g, 0x3f, sizeof(g));
-        // queue<pair<int, int>> q;
-        // int si = 0, sj = 0;
-        // for (int i = 0; i < m; ++i) {
-        //     for (int j = 0; j < n; ++j) {
-        //         auto c = land[i][j];
-        //         if (c == "*") {
-        //             q.emplace(i, j);
-        //         } else if (c == "S") {
-        //             si = i;
-        //             sj = j;
-        //         }
-        //     }
-        // }
-        // int dirs[5] = {-1, 0, 1, 0, -1};
-        // for (int t = 0; !q.empty(); ++t) {
-        //     for (int k = q.size(); k; --k) {
-        //         auto [i, j] = q.front();
-        //         q.pop();
-        //         g[i][j] = t;
-        //         for (int d = 0; d < 4; ++d) {
-        //             int x = i + dirs[d], y = j + dirs[d + 1];
-        //             if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y]) {
-        //                 bool empty = land[x][y] == ".";
-        //                 bool start = land[x][y] == "S";
-        //                 if (empty || start) {
-        //                     vis[x][y] = true;
-        //                     q.emplace(x, y);
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-        
-//         memset(vis, false, sizeof(vis));
-//         q.emplace(si, sj);
-//         vis[si][sj] = true;
+
+        memset(vis, false, sizeof(vis));
+        q.emplace(si, sj);
+        vis[si][sj] = true;
         
 //         for(int t = 0; !q.empty(); ++t) {
 //             for(int k = q.size(); k; --k) {
@@ -99,9 +61,9 @@ public:
 //             }
 //         }
         
-        q.emplace(si, sj);
-        memset(vis, false, sizeof(vis));
-        vis[si][sj] = true;
+        // q.emplace(si, sj);
+        // memset(vis, false, sizeof(vis));
+        // vis[si][sj] = true;
         for (int t = 0; !q.empty(); ++t) {
             for (int k = q.size(); k; --k) {
                 auto [i, j] = q.front();
