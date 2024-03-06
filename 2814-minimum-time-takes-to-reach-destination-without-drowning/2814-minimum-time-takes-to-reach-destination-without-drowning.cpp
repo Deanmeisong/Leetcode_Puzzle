3f0c83ad9a1e95d91aba7efbb4d7828ecc5f69fe
Deanmeisong <deanmeisong@gmail.com>
2024-03-06 22:29:33 +1100

    Time: 251 ms (66.67%), Space: 163.1 MB (83.33%) - LeetHub

--- 2814-minimum-time-takes-to-reach-destination-without-drowning/2814-minimum-time-takes-to-reach-destination-without-drowning.cpp
+++ 2814-minimum-time-takes-to-reach-destination-without-drowning/2814-minimum-time-takes-to-reach-destination-without-drowning.cpp
@@ -1,53 +1,16 @@
 class Solution {
 public:
     int minimumSeconds(vector<vector<string>>& land) {
-//         int m = land.size(), n = land[0].size();
-//         int g[m][n];
-//         bool vis[m][n];
-//         memset(g, 0x3f, sizeof(g));
-//         memset(vis, false, sizeof(vis));
-//         int si = 0, sj = 0;
-//         queue<pair<int, int>> q;
-        
-//         for(int i = 0; i < m; ++i)
-//             for(int j = 0; j < n; ++j) {
-//                 auto c = land[i][j];
-//                 if (c == "*") {
-//                     q.emplace(i, j);
-//                 } else if (c == "S") {
-//                     si = i;
-//                     sj = j;
-//                 }
-//             }
-        
-//         int dirs[5] = {-1, 0, 1, 0, -1};
-//         for(int t = 0; !q.empty(); ++t) {
-//             for(int k = q.size(); k; --k) {
-//                 auto [i, j] = q.front(); q.pop(); g[i][j] = t;
-//                 for(int d = 0; d < 4; ++d) {
-//                     int x = i + dirs[d]; int y = j + dirs[d+1];
-//                     if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y]) {
-//                         bool empty = land[x][y] == ".";
-//                         bool start = land[x][y] == "S";
-//                         if (empty || start) {
-//                             vis[x][y] = true;
-//                             q.emplace(x, y);
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-        
-        
-         int m = land.size(), n = land[0].size();
-        bool vis[m][n];
+        int m = land.size(), n = land[0].size();
         int g[m][n];
-        memset(vis, false, sizeof(vis));
+        bool vis[m][n];
         memset(g, 0x3f, sizeof(g));
-        queue<pair<int, int>> q;
+        memset(vis, false, sizeof(vis));
         int si = 0, sj = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        queue<pair<int, int>> q;
+        
+        for(int i = 0; i < m; ++i)
+            for(int j = 0; j < n; ++j) {
                 auto c = land[i][j];
                 if (c == "*") {
                     q.emplace(i, j);
@@ -56,15 +19,13 @@ public:
                     sj = j;
                 }
             }
-        }
+        
         int dirs[5] = {-1, 0, 1, 0, -1};
-        for (int t = 0; !q.empty(); ++t) {
-            for (int k = q.size(); k; --k) {
-                auto [i, j] = q.front();
-                q.pop();
-                g[i][j] = t;
-                for (int d = 0; d < 4; ++d) {
-                    int x = i + dirs[d], y = j + dirs[d + 1];
+        for(int t = 0; !q.empty(); ++t) {
+            for(int k = q.size(); k; --k) {
+                auto [i, j] = q.front(); q.pop(); g[i][j] = t;
+                for(int d = 0; d < 4; ++d) {
+                    int x = i + dirs[d]; int y = j + dirs[d+1];
                     if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y]) {
                         bool empty = land[x][y] == ".";
                         bool start = land[x][y] == "S";
@@ -76,6 +37,45 @@ public:
                 }
             }
         }
+        
+        
+        //  int m = land.size(), n = land[0].size();
+        // bool vis[m][n];
+        // int g[m][n];
+        // memset(vis, false, sizeof(vis));
+        // memset(g, 0x3f, sizeof(g));
+        // queue<pair<int, int>> q;
+        // int si = 0, sj = 0;
+        // for (int i = 0; i < m; ++i) {
+        //     for (int j = 0; j < n; ++j) {
+        //         auto c = land[i][j];
+        //         if (c == "*") {
+        //             q.emplace(i, j);
+        //         } else if (c == "S") {
+        //             si = i;
+        //             sj = j;
+        //         }
+        //     }
+        // }
+        // int dirs[5] = {-1, 0, 1, 0, -1};
+        // for (int t = 0; !q.empty(); ++t) {
+        //     for (int k = q.size(); k; --k) {
+        //         auto [i, j] = q.front();
+        //         q.pop();
+        //         g[i][j] = t;
+        //         for (int d = 0; d < 4; ++d) {
+        //             int x = i + dirs[d], y = j + dirs[d + 1];
+        //             if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y]) {
+        //                 bool empty = land[x][y] == ".";
+        //                 bool start = land[x][y] == "S";
+        //                 if (empty || start) {
+        //                     vis[x][y] = true;
+        //                     q.emplace(x, y);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
         
 //         memset(vis, false, sizeof(vis));
 //         q.emplace(si, sj);
