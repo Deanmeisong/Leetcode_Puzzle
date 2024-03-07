74be08c7a21fcaca62732d22048c5fdcf096e854
Deanmeisong <deanmeisong@gmail.com>
2024-03-07 22:11:51 +1100

    Time: 1019 ms (55.98%), Space: 427.9 MB (5.66%) - LeetHub

--- 2836-maximize-value-of-function-in-a-ball-passing-game/2836-maximize-value-of-function-in-a-ball-passing-game.cpp
+++ 2836-maximize-value-of-function-in-a-ball-passing-game/2836-maximize-value-of-function-in-a-ball-passing-game.cpp
@@ -2,10 +2,9 @@ using LL = long long;
 class Solution {
 public:
     long long getMaxFunctionValue(vector<int>& receiver, long long k) {
-        int n = receiver.size();
-        int m = ceil(log(k)/log(2));
-        vector<vector<LL>> dp(n + 1, vector<LL>(m + 1));
-        vector<vector<LL>> pos(n + 1, vector<LL>(m + 1));
+        int n = receiver.size(); int m = ceil(log(k)/log(2));
+        vector<vector<LL>> dp(n+1, vector<LL>(m+1));
+        vector<vector<LL>> pos(n+1, vector<LL>(m+1));
         for(int i = 0; i < n; ++i) {
             dp[i][0] = receiver[i];
             pos[i][0] = receiver[i];
@@ -16,18 +15,19 @@ public:
                 dp[i][j] = dp[i][j-1] + dp[pos[i][j-1]][j-1];
             }
         vector<int> bits;
-        for(int i = 0; i <= m; ++i)
+        for(int i = 0; i <= m; ++i) {
             if((k>>i)&1) bits.push_back(i);
-        LL ret = 0;
+        }
+        LL res = 0;
         for(int i = 0; i < n; ++i) {
-            LL val = i;
             int cur = i;
+            LL val = i;
             for(int j : bits) {
                 val += dp[cur][j];
                 cur = pos[cur][j];
             }
-            ret = max(ret, val);
+            res = max(res, val);
         }
-        return ret;
+        return res;
     }
 };
