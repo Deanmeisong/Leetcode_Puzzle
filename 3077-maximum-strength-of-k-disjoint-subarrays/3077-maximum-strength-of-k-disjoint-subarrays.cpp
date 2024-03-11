a06451bd3e03bd6e1d5b88982d343aa2d6c5038a
Deanmeisong <deanmeisong@gmail.com>
2024-03-12 00:23:00 +1100

    Time: 1601 ms (28.57%), Space: 562.3 MB (28.57%) - LeetHub

--- 3077-maximum-strength-of-k-disjoint-subarrays/3077-maximum-strength-of-k-disjoint-subarrays.cpp
+++ 3077-maximum-strength-of-k-disjoint-subarrays/3077-maximum-strength-of-k-disjoint-subarrays.cpp
@@ -1,24 +1,22 @@
 using LL = long long;
 class Solution {
 public:
-    vector<vector<vector<LL>>> dp;
-    LL dfs(int i, LL k, bool started, vector<int>& nums) {
-        if(k == 0) return 0;
-        if(i == nums.size()) {
-            if(k == 1 && started) return 0;
-            return -1e15;
-        }
-        if(dp[i][k][started] != -1e16) return dp[i][k][started];
-        LL ret = -1e15;
-        if(started) ret = max(ret,dfs(i, k-1, false, nums));
-        LL cur = k * ((k%2 == 1) ? 1 : -1) * nums[i];
-        ret = max(ret, cur + dfs(i+1, k, true, nums));
-        if(!started) ret = max(ret, dfs(i+1, k, false, nums));
-        return dp[i][k][started] = ret;
-        
-    }
     long long maximumStrength(vector<int>& nums, int k) {
-        dp = vector<vector<vector<LL>>>(nums.size(), vector<vector<LL>>(k+1, vector<LL>(2, -1e16)));
-        return dfs(0, (LL)k, false, nums);
+        int n = nums.size();
+        nums.insert(nums.begin(), 0);
+        vector<vector<vector<LL>>>dp(n+1, vector<vector<LL>>(k+1, vector<LL>(2, LLONG_MIN/3)));
+        for(int i = 0; i <= n; ++i)dp[i][0][0] = 0;
+        for(int i = 1; i <= n; ++i)
+            for(int j = 1; j <= k; ++j) {
+                if(j%2 == 0) {
+                    dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]);
+                    dp[i][j][1] = max(dp[i-1][j][1], max(dp[i-1][j-1][0], dp[i-1][j-1][1])) - (LL)(k-j+1)*nums[i];
+                } else {
+                    dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]);
+                    dp[i][j][1] = max(dp[i-1][j][1], max(dp[i-1][j-1][0], dp[i-1][j-1][1])) + (LL)(k-j+1)*nums[i];
+                }
+                      
+            }
+        return max(dp[n][k][0], dp[n][k][1]);
     }
 };
