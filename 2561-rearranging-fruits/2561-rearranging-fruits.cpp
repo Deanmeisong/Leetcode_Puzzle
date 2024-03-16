468175e1c37d7132750e816410aab1fa13ea0ac4
Deanmeisong <deanmeisong@gmail.com>
2024-03-16 16:39:02 +1100

    Time: 173 ms (45.19%), Space: 89.3 MB (80.74%) - LeetHub

--- 2561-rearranging-fruits/2561-rearranging-fruits.cpp
+++ 2561-rearranging-fruits/2561-rearranging-fruits.cpp
@@ -6,13 +6,16 @@ public:
         for(int x : basket2) --Map[x];
         int minV = Map.begin()->first;
         vector<int> arr;
-        for(auto [k, v] : Map) {
+        for(auto [k,v] : Map) {
             if(v % 2 != 0) return -1;
-            for(int i = 0; i < abs(v/2); ++i)
-                arr.push_back(k);
+            if(v > 0) {
+                for(int i = 0; i < v/2; ++i)
+                    arr.push_back(k);
+            } else {
+                for(int i = 0; i < abs(v)/2; ++i)
+                    arr.push_back(k);
+            }
         }
-        
-        sort(arr.begin(), arr.end());
         long long ans = 0;
         for(int i = 0; i < arr.size()/2; ++i)
             ans += min(2*minV, arr[i]);
