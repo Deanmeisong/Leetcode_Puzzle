fd5a4ddbc104c17bf64000934d3e38499faa9155
Deanmeisong <deanmeisong@gmail.com>
2024-03-18 12:05:03 +1100

    Time: 82 ms (78.20%), Space: 57 MB (70.86%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -4,23 +4,15 @@ public:
     long long minimumReplacement(vector<int>& nums) {
         LL ret = 0;
         for(int i = nums.size() - 2; i >= 0; --i) {
-            LL x = nums[i+1]; LL y = nums[i];
-            if(x >= y) continue;
-            LL k = y/x; LL d = y%x;
-            if(d == 0) {
-                ret += k-1;
+            int y = nums[i]; int x = nums[i+1];
+            if(y <= x) continue;
+            if(y%x == 0) {
+                ret += y/x-1;
                 nums[i] = x;
-                continue;
+            } else {
+                ret += y/x;
+                nums[i] = y/(y/x+1);
             }
-            
-            // d+k*p <= x-p
-            LL p = (x-d)/(k+1);
-            LL x2 = x - p;
-            LL d2 = d + p*k;
-            if(d2 < x2) nums[i] = x2-1;
-            else nums[i] = d2;
-            ret += k;
-            
         }
         return ret;
     }
