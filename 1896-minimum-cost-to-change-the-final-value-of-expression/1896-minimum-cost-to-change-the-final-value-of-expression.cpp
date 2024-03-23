d4c050779c9abc83784cd1af82b170e903f35191
Deanmeisong <deanmeisong@gmail.com>
2024-03-23 15:53:36 +1100

    Time: 34 ms (94.44%), Space: 15.1 MB (97.22%) - LeetHub

--- 1896-minimum-cost-to-change-the-final-value-of-expression/1896-minimum-cost-to-change-the-final-value-of-expression.cpp
+++ 1896-minimum-cost-to-change-the-final-value-of-expression/1896-minimum-cost-to-change-the-final-value-of-expression.cpp
@@ -6,46 +6,43 @@ public:
         else return a.first | b.first;
     }
     
-    int evenFlip(char op, pair<int,int>& a, pair<int,int>& b) {
+    int evalFlip(char op, pair<int,int>& a, pair<int,int>& b) {
         if(op == '#') return b.second;
-        else if(op == '&') 
+        
+        if(op == '&') // 
         {
             if(a.first + b.first == 2) return min(a.second, b.second);
             else if(a.first + b.first == 1) return 1;
             else return min(a.second, b.second) + 1;
         }
-        else //&
+        else // | === 1 &   
         {
-            if(a.first + b.first == 2) return min(a.second, b.second)+1;
+            if (a.first + b.first == 2) return min(a.second, b.second) + 1;
             else if(a.first + b.first == 1) return 1;
             else return min(a.second, b.second);
         }
+ 
     }
     
     int minOperationsToFlip(string expression) {
-        stack<pair<int,int>> s1; stack<char> s2; pair<int,int> cur = {-1,-1}; char op = '#';
-        
-        for(auto ch : expression) {
+        stack<pair<int,int>> s1; stack<char> s2; char op = '#'; pair<int,int> cur = {-1,-1};
+        for(char ch : expression) {
             if(ch == '&' || ch == '|') op = ch;
-            else if(ch == '0' || ch == '1')
-            {
+            else if(ch == '0' || ch == '1') {
                 pair<int,int> nxt = {ch-'0', 1};
                 int val = evalVal(op, cur, nxt);
-                int flip = evenFlip(op, cur, nxt);
+                int flip = evalFlip(op, cur, nxt);
                 cur = {val, flip};
-            }
-            else if(ch == '(')
-            {
+            } else if(ch == '(') {
                 s1.push(cur);
                 s2.push(op);
                 cur = {-1, -1};
                 op = '#';
-            }
-            else {
-                auto last = s1.top(); s1.pop();
+            } else if(ch == ')') {
+                pair<int,int> last = s1.top(); s1.pop();
                 char op = s2.top(); s2.pop();
                 int val = evalVal(op, last, cur);
-                int flip = evenFlip(op, last, cur);
+                int flip = evalFlip(op, last, cur);
                 cur = {val, flip};
             }
         }
