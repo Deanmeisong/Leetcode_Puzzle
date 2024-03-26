a7de1134fbc0c5f16f02cfb4393c619acb0db1db
Deanmeisong <deanmeisong@gmail.com>
2024-03-26 20:43:33 +1100

    Time: 201 ms (31.47%), Space: 106.7 MB (20.82%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,10 +1,13 @@
 class Solution {
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        return a[1] < b[1];
+    }
 public:
     int jobScheduling(vector<int>& s, vector<int>& e, vector<int>& p) {
         int n = s.size();
         vector<vector<int>> js;
         for(int i = 0; i < n; ++i) js.push_back({s[i], e[i], p[i]});
-        sort(js.begin(), js.end(), [](auto& a, auto& b) {return a[1] < b[1];});
+        sort(js.begin(), js.end(),cmp);
         map<int,int> dp;
         dp[-1] = 0;
         int ans = 0;
