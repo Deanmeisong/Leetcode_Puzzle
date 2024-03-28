2e8eec4c0bdffa1504031744c6513ee2f71c463b
Deanmeisong <deanmeisong@gmail.com>
2024-03-28 14:48:00 +1100

    Time: 293 ms (61.16%), Space: 16.6 MB (88.43%) - LeetHub

--- 1259-handshakes-that-dont-cross/1259-handshakes-that-dont-cross.py
+++ 1259-handshakes-that-dont-cross/1259-handshakes-that-dont-cross.py
@@ -1,14 +1,10 @@
-class Solution(object):
-    def numberOfWays(self, numPeople):
-        """
-        :type numPeople: int
-        :rtype: int
-        """
+class Solution:
+    def numberOfWays(self, numPeople: int) -> int:
         m = 1000000007
         dp = [0] * (numPeople//2 + 1)
         dp[0] = 1
         for i in range(1, numPeople//2 + 1):
-            for j in range(i):
-                dp[i] += dp[j] * dp[i-j-1]
+            for j in range(0, i):
+                dp[i] += dp[j] * dp[i-j-1] % m
                 dp[i] %= m
-        return dp[numPeople // 2]
+        return dp[numPeople//2]
