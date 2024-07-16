7761868d96cd18fd200ee79d94f113a67abab32c
dean <deanmeisong@gmail.com>
2024-07-16 16:21:45 +1000

    Time: 66 ms (72.93%), Space: 55.6 MB (35.00%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.ts
@@ -0,0 +1,13 @@
+function longestOnes(nums: number[], k: number): number {
+    let l = 0; let r = 0; let ans = 0; let count = 0;
+    while(r < nums.length) {
+        if(nums[r] == 0) ++count;
+        while(count > k) {
+            if(nums[l] == 0) --count;
+            ++l;
+        }
+        ans = Math.max(ans, r-l+1);
+        ++r;
+    }
+    return ans;
+};
