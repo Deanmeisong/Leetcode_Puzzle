5ec1215ce08363e2d9d81e0134687da29517df97
dean <deanmeisong@gmail.com>
2024-08-18 12:48:15 +1000

    Time: 173 ms (42.26%), Space: 80 MB (5.32%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> diff(n+2, 0);
+        for(auto book : bookings) {
+            diff[book[0]] += book[2];
+            diff[book[1]+1] -= book[2];
+        }
+        vector<int> ret;
+        int count = 0;
+        for(int i = 1; i <= n; ++i) {
+            count += diff[i];
+            ret.push_back(count);
+        }
+        return ret;
+    }
+};
