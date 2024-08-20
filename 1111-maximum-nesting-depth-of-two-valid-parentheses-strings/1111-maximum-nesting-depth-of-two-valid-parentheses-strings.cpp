b471c3e25cf227d58867775b2e0f93e4450b3c8d
dean <deanmeisong@gmail.com>
2024-08-20 16:50:17 +1000

    Time: 0 ms (100.00%), Space: 8.8 MB (71.35%) - LeetHub

--- 1111-maximum-nesting-depth-of-two-valid-parentheses-strings/1111-maximum-nesting-depth-of-two-valid-parentheses-strings.cpp
+++ 1111-maximum-nesting-depth-of-two-valid-parentheses-strings/1111-maximum-nesting-depth-of-two-valid-parentheses-strings.cpp
@@ -3,18 +3,9 @@ public:
     vector<int> maxDepthAfterSplit(string seq) {
         int n = seq.size();
         vector<int> ans(n);
-        for (int i = 0, x = 0; i < n; ++i) {
-            if (seq[i] == '(') {
-                cout<<"before x: "<<x<<" || ";
-                ans[i] = x++ & 1;
-                cout<<"after x: "<<x<<" || ";
-                cout<<"ansi: "<<ans[i]<<endl;
-            } else {
-                cout<<" ====== before x: "<<x<<" || ";
-                ans[i] = --x & 1;
-                cout<<" ====== after x: "<<x<<" || ";
-                cout<<" ====== ansi: "<<ans[i]<<endl;
-            }
+        for(int i=0, x=0; i < n; ++i) {
+            if(seq[i] == '(') ans[i] = x++ & 1;
+            else ans[i] = --x & 1;
         }
         return ans;
     }
